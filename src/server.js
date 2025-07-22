eca3bba29fb20df429ceee46e1267bb5116a1876
naufal <naufalsatrioputra@gmail.com>
2025-07-22 19:24:41 +0700

    add authentications

--- src/server.js
+++ src/server.js
@@ -2,18 +2,27 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const ClientError = require('./exceptions/ClientError');
 
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NoteService');
 const NotesValidator = require('./validator/notes');
 
+// users
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersSevice');
 const UsersValidator = require('./validator/users');
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
 
 const init = async () => {
 
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -38,6 +47,15 @@ const init = async () => {
     options: {
       service: usersService,
       validator: UsersValidator,
+    },
+  },
+  {
+    plugin: authentications,
+    options: {
+      authenticationsService,
+      usersService,
+      tokenManager: TokenManager,
+      validator: AuthenticationsValidator,
     },
   },
 ]);
