ddcceba84b1c3fd877c292495e7a49f7b257108c
naufal <naufalsatrioputra@gmail.com>
2025-07-26 15:55:22 +0700

    debug

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const NotesValidator = require('./validator/notes');
 
 // users
 const users = require('./api/users');
-const UsersService = require('./services/postgres/UsersSevice');
+const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
 // authentications
@@ -20,12 +20,19 @@ const AuthenticationsService = require('./services/postgres/AuthenticationsServi
 const TokenManager = require('./tokenize/TokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
 
-const init = async () => {
+// collaborations
+const collaborations = require('./api/collaborations');
+const CollaborationsService = require('./services/postgres/CollaborationsService');
+const CollaborationsValidator = require('./validator/collaborations');
 
+const init = async () => {
+  
   const notesService = new NotesService();
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-
+  const collaborationsService = new CollaborationsService();
+  
+ 
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -35,15 +42,15 @@ const init = async () => {
       },
     },
   });
-
-    // registrasi plugin eksternal
+ 
+  // registrasi plugin eksternal
   await server.register([
     {
       plugin: Jwt,
     },
   ]);
  
-  // mendefinisikan strategy autentikasi jwt
+  // mendefinisikan strategy otentikasi jwt
   server.auth.strategy('notesapp_jwt', 'jwt', {
     keys: process.env.ACCESS_TOKEN_KEY,
     verify: {
@@ -59,52 +66,43 @@ const init = async () => {
       },
     }),
   });
-
+ 
   await server.register([
     {
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  },
-  {
-    plugin: users,
-    options: {
-      service: usersService,
-      validator: UsersValidator,
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
     },
-  },
-  {
-    plugin: authentications,
-    options: {
-      authenticationsService,
-      usersService,
-      tokenManager: TokenManager,
-      validator: AuthenticationsValidator,
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
     },
-  },
-]);
-
-  server.ext('onPreResponse', (request, h) => {
-   // mendapatkan konteks response dari request
-   const { response } = request;
+    {
+      plugin: collaborations,
+      options: {
+        collaborationsService,
+        notesService,
+        validator: CollaborationsValidator,
+      },
+    },
+  ]);
  
-   // penanganan client error secara internal.
-   if (response instanceof ClientError) {
-     const newResponse = h.response({
-       status: 'fail',
-       message: response.message,
-     });
-     newResponse.code(response.statusCode);
-     return newResponse;
-   }
-     
-   return h.continue;
-  });
-
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
+ 
 init();
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -10,23 +10,22 @@ class UsersService {
         this._pool = new Pool();
     }
 
-   
     async addUser({ username, password, fullname }) {
-    await this.verifyNewUsername(username);
+        await this.verifyNewUsername(username);
 
-    const id = `user-${nanoid(16)}`;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const query = {
-        text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
-        values: [id, username, hashedPassword, fullname],
-    };
+        const id = `user-${nanoid(16)}`;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const query = {
+            text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+            values: [id, username, hashedPassword, fullname],
+        };
 
-    const result = await this._pool.query(query);
+        const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
-        throw new InvariantError('User gagal ditambahkan');
-    }
-    return result.rows[0].id;
+        if (!result.rows.length) {
+            throw new InvariantError('User gagal ditambahkan');
+        }
+        return result.rows[0].id;
     }
 
     async getUserById(userId) {
